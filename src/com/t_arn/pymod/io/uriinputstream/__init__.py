5c8a0a8a9b7445c4d740d18534143097af263958
sw@t-arn.com <sw@t-arn.com>
2023-04-26 23:31:13 +0200

    cross platform test

---
+++ src/com/t_arn/pymod/io/uriinputstream/__init__.py
@@ -0,0 +1,6 @@
+﻿import toga
+
+if toga.platform.current_platform == "android":
+    from .android import UriInputStream
+if toga.platform.current_platform == "windows":
+    from .windows import UriInputStream
---
+++ src/com/t_arn/pymod/io/uriinputstream/android.py
@@ -0,0 +1,11 @@
+﻿class UriInputStream:
+    uri = None
+    
+    def __init__(self, uri):
+        self.uri = uri
+    # __init__
+    
+    def read(self, size):
+        return f"{self.uri}::read({size})"
+    # read
+# UriInputStream
