de1aeb8005a18d48a1713e5c31f9cf6069c8d937
Tom Arn <sw@t-arn.com>
2024-02-20 20:43:16 +0100

    added template for derived Toga Widgets

---
+++ templates/newwidget/android.py
@@ -0,0 +1,11 @@
+import toga
+
+
+class NewWidgetImpl:
+    def __init__(self, interface):
+        self.interface = interface
+        self.app = toga.App.app
+
+
+version = "1.0.0"
+version_date = "2024-02-20 - 2024-03-20"
---
+++ templates/newwidget/core.py
@@ -0,0 +1,27 @@
+# from .base import Widget
+from toga import Widget
+
+from ... import system
+
+
+if system.get_platform() == "android":
+    from .android import NewWidgetImpl
+elif system.get_platform() == "windows":
+    from .windows import NewWidgetImpl
+else:
+    raise NotImplementedError(f"NewWidget is not implemented for {system.get_platform()}")
+
+
+#class OriginalClass(Widget):
+class NewWidget(Widget):
+    def __init__(
+        self,
+        id,
+        style,
+        # other params
+      ):
+        """Class documentation
+        """
+        super().__init__(id=id, style=style)
+
+ 
